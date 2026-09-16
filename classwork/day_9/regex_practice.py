635fdd04ed2ad35ce41fc4ce6677629e92b812fa
Abagail <abagaildeweese90@gmail.com>
2026-09-16 11:12:01 -0500

    save

--- classwork/day_9/regex_practice.py
+++ classwork/day_9/regex_practice.py
@@ -1,6 +1,2 @@
-import re                                             # loads Python's regex toolkit
 
-with open("../../data/gutenberg/alice.txt", encoding="utf-8") as f:  # opens alice.txt for reading
-    text = f.read()                                   # reads the whole file into one string, called `text`
-                                              # reads the whole file into one string, called `text`
 
