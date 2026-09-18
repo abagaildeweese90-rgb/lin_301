88184840b775efbbf826d148d2dab945eb99bf82
Abagail <abagaildeweese90@gmail.com>
2026-09-18 11:05:04 -0500

    commit

--- classwork/day_9/regex_practice.py
+++ classwork/day_9/regex_practice.py
@@ -1,2 +1,5 @@
+import re                                                     
 
+with open("../../data/gutenberg/alice.txt", encoding="utf-8") as f:  
+    text = f.read()                                      
 
