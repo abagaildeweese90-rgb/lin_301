78b1bc91947d4ca86e034a2cc3a958c875b1e44a
Abagail <abagaildeweese90@gmail.com>
2026-09-23 11:19:39 -0500

    commit

---
+++ classwork/day_13/Prac.py
@@ -0,0 +1,7 @@
+orwell_dict = {
+    "Animal Farm": 1945,
+    "Nineteen Eighty-Four": 1949,
+    "Burmese Days": 1934,
+    "Keep the Aspidistra Flying": 1936,
+    "Coming Up for Air": 1939}
+orwell_dict["Animal Farm"]
